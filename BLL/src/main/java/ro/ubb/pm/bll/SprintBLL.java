9fc52b427d034e62f96035e872624d1fa117416e
Luke <lucabalogh10@gmail.com>
2021-11-18 22:58:23 +0200

    Updated DAL and BLL

--- BLL/src/main/java/ro/ubb/pm/bll/SprintBLL.java
+++ BLL/src/main/java/ro/ubb/pm/bll/SprintBLL.java
@@ -1,49 +1,16 @@
 package ro.ubb.pm.bll;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;;
 import org.springframework.stereotype.Component;
 import ro.ubb.pm.dal.SprintsRepository;
-import ro.ubb.pm.model.Sprint;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
-@EntityScan(basePackages = "ro.ubb.pm.dal")
 public class SprintBLL {
     private SprintsRepository sprintsRepository;
-
-  /*  public SprintBLL(SprintsRepository sprintsRepository) {
-=======
-public class SprintBLL {
-    private SprintsRepository sprintsRepository;
-
-    @Autowired
-    public void setTasksRepository(SprintsRepository sprintsRepository) {
->>>>>>> fe296bedc5aa441bc8c50560d91518399b11a694
-        this.sprintsRepository = sprintsRepository;
-    }
-*/
 
     @Autowired
     public void setSprintsRepository(SprintsRepository sprintsRepository){
         this.sprintsRepository= sprintsRepository;
-    }
-    public List<Sprint> getAllSprints(){
-        return sprintsRepository.findAll();
-    }
-
-    public List<Sprint> getAllSprintsForUser(int id){
-        List<Sprint> sprints = new ArrayList<>();
-
-        for(Sprint s: getAllSprints())
-            if(s.getId() == id)
-                sprints.add(s);
-
-        return sprints;
     }
 }
--- DAL/src/main/java/ro/ubb/pm/dal/TasksRepository.java
+++ DAL/src/main/java/ro/ubb/pm/dal/TasksRepository.java
@@ -1,9 +1,16 @@
 package ro.ubb.pm.dal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ro.ubb.pm.model.Task;
 
+import java.util.List;
+
 @Repository
 public interface TasksRepository extends JpaRepository<Task, Integer> {
+
+    @Query("FROM Task task WHERE task.userStory.id =: userStoryId")
+    List<Task> findAllByUserStoryId(int userStoryId);
 }
